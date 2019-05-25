b2e7d557137c5983e967180a9555a08d12be7a1a
Łukasz Hendrysiak <lukaszhendrysiak@localhost.localdomain>
2019-05-25 20:08:21 +0200

    Created project in 0.1 version

---
+++ main.js
@@ -0,0 +1,39 @@
+const images = [...document.querySelectorAll('.image-small')];
+
+const burgerMenu = document.querySelector('.burger_menu');
+const menu = document.querySelector('.right');
+const presentation = document.querySelector('.image-big');
+const title = document.querySelector('.change');
+
+function openMenu() {
+    menu.classList.toggle('active')
+    if (menu.classList.contains('active')) {
+        burgerMenu.textContent = 'X'
+    } else {
+        burgerMenu.textContent = '+'
+    }
+}
+
+function changeImage(e) {
+    images.forEach(image => {
+        if (image.lastElementChild.classList.contains('active')) {
+            image.lastElementChild.classList.remove('active')
+        }
+    })
+    console.log(e.target)
+    presentation.innerHTML = '';
+    title.textContent = '';
+    title.textContent = e.target.alt;
+    e.target.classList.add('active');
+    presentation.innerHTML = '';
+    let image = document.createElement("img");
+    image.src = e.target.src;
+    image.alt = e.target.alt;
+    image.classList.add('image-great');
+
+    presentation.appendChild(image);
+
+}
+
+burgerMenu.addEventListener('click', openMenu)
+images.forEach(image => image.addEventListener('click', changeImage))
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Simple gallery v0.1
+
+Project created with using flexbox, ES6, HTML5, CSS3
